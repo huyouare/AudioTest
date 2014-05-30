89efa5398ec6bed702c428d8c4d27e18be9de2c5
Jesse Hu <huyouare@gmail.com>
2014-05-29 20:49:54 -0400

    Initial Commit

---
+++ AudioTest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  AudioTest
+//
+//  Created by Jesse Hu on 5/29/14.
+//  Copyright (c) 2014 Jesse Hu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
